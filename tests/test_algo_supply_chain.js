bdd3f1a19102d3124896fa77e04aae6e4240e7cc
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-12-02 14:01:03 +0200

    Add test for logging supply chain data to algorand blockchain

---
+++ tests/test_algo_supply_chain.js
@@ -0,0 +1,136 @@
+// Test adding supply chain operational data to Algorand blockchain
+// Usage from project root - $node  -r dotenv/config tests/test_algo_supply_chain.js
+
+const algosdk = require('algosdk');
+
+//------Import account mnemonics------\\
+var account1_mnemonic = process.env.ACCOUNT1_MNEMONIC;
+var account2_mnemonic = process.env.ACCOUNT2_MNEMONIC;
+
+console.log("Account Mnemonic 1 from user = " + account1_mnemonic);
+console.log("Account Mnemonic 2 from user = " + account2_mnemonic);
+
+//------/Import account mnemonics------\\
+
+//------Recover algorand accounts from mnemonic/secret phrase------\\
+
+console.log("Now attempting to recover accounts using user supplied secret phrases...");
+
+var recoveredAccount1 = algosdk.mnemonicToSecretKey(account1_mnemonic);
+var isValid = algosdk.isValidAddress(recoveredAccount1.addr);
+console.log("Account recovered: " + isValid);
+var account1 = recoveredAccount1.addr;
+console.log("Account 1 = " + account1);
+
+var recoveredAccount2 = algosdk.mnemonicToSecretKey(account2_mnemonic);
+var isValid = algosdk.isValidAddress(recoveredAccount2.addr);
+console.log("Account recovered: " + isValid);
+var account2 = recoveredAccount2.addr;
+console.log("Account 2 = " + account2);
+
+console.log("");
+
+//------/Recover algorand accounts from mnemonic/secret phrase------\\
+
+//------Setup algod connection------\\
+
+console.log("Now setting up algod connection...");
+
+// configure  algod client connection parameters i.e. connection to algo network (this can either be to sandbox or via a 3rd party service)
+
+// this token is not the same as a digital asset, rather it is API Key (token): which is defined as an object. The token is used to
+// identify the source of a connection / give you permission to access an algorand node. In the sandbox, the default token is aaa...
+// Example use of token is when querying KMD for list of wallets, the first will work but the second has invalid key and fails
+// $curl localhost:4002/v1/wallets -H "X-KMD-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// $curl localhost:4002/v1/wallets -H "X-KMD-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaB"
+
+// This is especially useful if you are connecting to the Algorand TestNet/BetaNet/MainNet via a 3rd party service i.e. a 3rd party node
+// e.g. https://developer.purestake.io/ (in the Ethereum space, this would be infura.io)
+// $curl -X GET "https://testnet-algorand.api.purestake.io/ps2/v2/status" -H "x-api-key: REPLACE_WITH_PURESTAKE_KEY"
+
+const environment = process.env.ENV // DEV, TESTNET or MAINNET
+console.log("ENV currently set to " + environment)
+
+if (environment == 'TESTNET') {
+    const token = {
+    'X-API-Key': process.env.TESTNET_ALGOD_API_KEY
+}
+    const server = process.env.TESTNET_ALGOD_SERVER; // PureStake "https://testnet-algorand.api.purestake.io/ps2" or AlgoExplorer "https://api.testnet.algoexplorer.io",
+    const port = process.env.TESTNET_ALGOD_PORT;
+} else if (environment == 'MAINNET') {
+    //if MAINNET
+    const token = {
+        'X-API-Key': process.env.MAINNET_ALGOD_API_KEY
+    }
+    const server = process.env.MAINNET_ALGOD_SERVER; // PureStake "https://testnet-algorand.api.purestake.io/ps2" or AlgoExplorer "https://api.testnet.algoexplorer.io",
+    const port = process.env.MAINNET_ALGOD_PORT;
+} else {
+    //if DEV
+    const token = process.env.DEV_ALGOD_API_KEY; // "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    const server = process.env.DEV_ALGOD_SERVER; // "http://localhost";
+    const port = process.env.DEV_ALGOD_PORT;
+}
+
+
+// Instantiate the algod wrapper
+let algodclient = new algosdk.Algodv2(token, server, port);
+
+//------/Setup aldod connection------\\
+
+//------Check algod status------\\
+
+console.log("Now checking algod status...");
+
+(async () => {
+    // call the status method from the algod client to check the details of your connection.
+    let status = (await algodclient.status().do());
+    console.log("Algorand network status: %o", status);
+})().catch(e => {
+    console.log(e);
+});
+
+//------/Check algod status------\\
+
+//------Send algos from one account to another and check balance------\\
+
+console.log("Now sending algos from one account 1 to account 2... Assumes account 1 has funds!");
+//If you use the async keyword before a function definition, you can then use await within the function.
+// When you await a promise, the function is paused in a non-blocking way until the promise settles.
+(async() => {
+    let params = await algodclient.getTransactionParams().do();
+    let txn = {
+        "from": recoveredAccount1.addr,
+        "to": recoveredAccount2.addr,
+        "fee": 1,
+        "amount": 1000000, // 1 algos
+        "amount": 0, // 0 algos
+        "firstRound": params.firstRound,
+        "lastRound": params.lastRound,
+        "genesisID": params.genesisID,
+        "genesisHash": params.genesisHash,
+        "note": new Uint8Array(0)
+    }
+    let signedTxn = algosdk.signTransaction(txn, recoveredAccount1.sk);
+    let sendTx = await algodclient.sendRawTransaction(signedTxn.blob).do();
+
+    console.log("Transaction: " + sendTx.txId);
+
+    console.log("Now checking balances...");
+    // get information on accounts e.g. balances
+    let account_info = (await algodclient.accountInformation(recoveredAccount1.addr).do());
+    let acct_string = JSON.stringify(account_info);
+    console.log("Account 1 Info: " + acct_string);
+    console.log("Balance of account 1: " + JSON.stringify(account_info.amount));
+
+    account_info = (await algodclient.accountInformation(recoveredAccount2.addr).do());
+    acct_string = JSON.stringify(account_info);
+    console.log("Account 2 Info: " + acct_string);
+    console.log("Balance of account 2: " + JSON.stringify(account_info.amount));
+
+    console.log("Now checking transactions...");
+
+
+})().catch( e => {
+    console.log(e)
+})
+//------/Send algos from one account to another and check balance------\\
