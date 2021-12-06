5b9a66644dfb9fe701dcb8632e3b4cef5146804b
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-12-07 00:05:29 +0200

    make script use dynamic environment variables instead of hardcoded

--- tests/test_algo_search_notes_indexer.js
+++ tests/test_algo_search_notes_indexer.js
@@ -5,19 +5,102 @@
 // Usage from project root - $node  -r dotenv/config tests/test_algo_search_notes_indexer.js
 
 const algosdk = require('algosdk');
-const indexer_token = "";
-const indexer_server = "http://localhost";
-const indexer_port = 8980;
+
+//------Import account mnemonics------\\
+var account1_mnemonic = process.env.ACCOUNT1_MNEMONIC;
+var account2_mnemonic = process.env.ACCOUNT2_MNEMONIC;
+
+console.log("Account Mnemonic 1 from user = " + account1_mnemonic);
+console.log("Account Mnemonic 2 from user = " + account2_mnemonic);
+
+//------/Import account mnemonics------\\
+
+//------Recover algorand accounts from mnemonic/secret phrase------\\
+
+console.log("Now attempting to recover accounts using user supplied secret phrases...");
+
+var recoveredAccount1 = algosdk.mnemonicToSecretKey(account1_mnemonic);
+var isValid = algosdk.isValidAddress(recoveredAccount1.addr);
+console.log("Account recovered: " + isValid);
+var account1 = recoveredAccount1.addr;
+console.log("Account 1 = " + account1);
+
+var recoveredAccount2 = algosdk.mnemonicToSecretKey(account2_mnemonic);
+var isValid = algosdk.isValidAddress(recoveredAccount2.addr);
+console.log("Account recovered: " + isValid);
+var account2 = recoveredAccount2.addr;
+console.log("Account 2 = " + account2);
+
+console.log("");
+
+//------/Recover algorand accounts from mnemonic/secret phrase------\\
+
+//------Setup indexer connection------\\
+
+console.log("Now setting up indexer connection...");
+
+// configure  indexer client connection parameters i.e. connection to algo network (this can either be to sandbox or via a 3rd party service)
+
+// this token is not the same as a digital asset, rather it is API Key (token): which is defined as an object. The token is used to
+// identify the source of a connection / give you permission to access an algorand node. In the sandbox, the default token is aaa...
+// Example use of token is when querying KMD for list of wallets, the first will work but the second has invalid key and fails
+// $curl localhost:4002/v1/wallets -H "X-KMD-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+// $curl localhost:4002/v1/wallets -H "X-KMD-API-Token: aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaB"
+
+// This is especially useful if you are connecting to the Algorand TestNet/BetaNet/MainNet via a 3rd party service i.e. a 3rd party node
+// e.g. https://developer.purestake.io/ (in the Ethereum space, this would be infura.io)
+// $curl -X GET "https://testnet-algorand.api.purestake.io/ps2/v2/status" -H "x-api-key: REPLACE_WITH_PURESTAKE_KEY"
+
+const environment = process.env.ENV; // DEV, TESTNET or MAINNET
+console.log("ENV currently set to " + environment);
+
+let token, server, port;
+
+if (environment == 'TESTNET') {
+    token = {
+    'X-API-Key': process.env.TESTNET_ALGOD_API_KEY
+}
+    server = process.env.TESTNET_ALGOINDEXER_SERVER; // PureStake https://testnet-algorand.api.purestake.io/idx2
+    port = process.env.TESTNET_ALGOD_PORT;
+} else if (environment == 'MAINNET') {
+    //if MAINNET
+    token = {
+        'X-API-Key': process.env.MAINNET_ALGOD_API_KEY
+    }
+    server = process.env.MAINNET_ALGOINDEXER_SERVER;
+    port = process.env.MAINNET_ALGOD_PORT;
+} else {
+    //if DEV
+    token = process.env.DEV_ALGOD_API_KEY; // "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    server = process.env.DEV_ALGOD_SERVER; // "http://localhost";
+    port = process.env.DEV_ALGOINDEXER_PORT; //8980
+}
 
 // Instantiate the indexer client wrapper
-let indexerClient = new algosdk.Indexer(indexer_token, indexer_server, indexer_port);
+let indexerClient = new algosdk.Indexer(token, server, port);
 
+//------/Setup indexer connection------\\
+
+//------Indexer get block (i.e. check status)------\\
+
+console.log("Now checking if indexer is available and looking up block 5...");
+(async () => {
+    let blockInfo = await indexerClient.lookupBlock(5).do()
+    console.log(blockInfo)
+})().catch(e => {
+    console.log(e);
+});
+
+//------/Indexer get block (i.e. check status)------\\
+
+//------Search transaction notes------\\
 (async () => {
     let names = '{"firstName":"John"';
     const enc = new TextEncoder();
     const note = enc.encode(names);
     const s = Buffer.from(note).toString("base64");
     let transactionInfo = await indexerClient.searchForTransactions()
+        .address(recoveredAccount2.addr) // only include transactions with this address in one of the transaction fields
         .minRound(10894697)
         .notePrefix(s).do();
     console.log("Information for Transaction search: " + JSON.stringify(transactionInfo, undefined, 2));
@@ -36,3 +119,5 @@ let indexerClient = new algosdk.Indexer(indexer_token, indexer_server, indexer_p
     console.log(e);
     console.trace();
 });
+
+//------/Search transaction notes------\\
