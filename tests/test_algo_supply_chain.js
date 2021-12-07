8508f7461c132d9b4e0d071697db6ee5e0697ddf
Julian Kanjere <julian.kanjere@jskdezign.com>
2021-12-07 22:29:11 +0200

    Add foodprint test harvest and storage data

--- tests/test_algo_supply_chain.js
+++ tests/test_algo_supply_chain.js
@@ -138,13 +138,15 @@ console.log("Now checking algod status...");
 
 //------/Check algod status------\\
 
-//------Send algos from one account to another and check balance------\\
+//------Log supply chain data (harvest) and check balance------\\
 
-console.log("Now sending algos from %s to %s... Assumes source account has funds!", recoveredAccount1.addr, recoveredAccount2.addr);
+console.log("Now logging supply chain harvest data from %s to %s... Assumes source account has funds to pay fees", recoveredAccount1.addr, recoveredAccount2.addr);
 //If you use the async keyword before a function definition, you can then use await within the function.
 // When you await a promise, the function is paused in a non-blocking way until the promise settles.
 (async() => {
     let params = await algodclient.getTransactionParams().do();
+    // see https://mumbai.polygonscan.com/tx/0xffbd57ca02f12666561b7789a09e29868f564b4344b8b319e74e0181658af40e vs
+    // https://testnet.algoexplorer.io/tx/X2V5FXDKXOEOSI3JD7XJVZFSETFMG5KPK3KANIXAFRQJ2D3QDCZA
     let supplyChainData = '{"logID":"9c6491af-74d8-43b1-acdd-83737cd6ec83", "logtype":"harvest", _supplierproduceID":"BGSM_Bergamot",' +
         '"_photoHash":"a066295322a9e6f2137d276b911d45362b206ce3749624908aaf06ede392d6e2",' +
         '"_geolocationFoodPrint":"Test", "actionTimeStamp":"Thu Nov 04 2021 13:54:00 GMT+0200 (South Africa Standard Time)",'+
@@ -201,5 +203,71 @@ console.log("Now sending algos from %s to %s... Assumes source account has funds
 })().catch( e => {
     console.log(e)
 })
-//------/Send algos from one account to another and check balance------\\
+//------/Log supply chain data (harvest) and check balance------\\
 
+//------Log supply chain data (handover/storage) and check balance------\\
+
+console.log("Now logging supply chain handover data from %s to %s... Assumes source account has funds to pay fees!", recoveredAccount1.addr, recoveredAccount2.addr);
+//If you use the async keyword before a function definition, you can then use await within the function.
+// When you await a promise, the function is paused in a non-blocking way until the promise settles.
+(async() => {
+    let params = await algodclient.getTransactionParams().do();
+    // see https://mumbai.polygonscan.com/tx/0x009c453c0fdb2509245377877c9adde687068effd9bf28859e000f87ac3b4751 vs
+    // https://testnet.algoexplorer.io/tx/4ESVS3Z5CGCARFXB64CWZKBVWK3VBHNQNIMZLMC5VGT653MFS7JA
+    let supplyChainData = '{"logID":"25ed364a-5a22-4796-9e42-c2496164e917", "previouslogID":"9c6491af-74d8-43b1-acdd-83737cd6ec83", ' +
+        '"logtype":"storage", "_otherID":"{supplierproduceID:BGSM_Bergamot, marketID:OZCFM}"' +
+        '"actionTimeStamp":"Thu Nov 04 2021 14:24:00 GMT+0200 (South Africa Standard Time)",'+
+        '"logDetail": "{description:FoodPrint Test, logTableName:foodprint_storage, logUser:adminjack@example.com, logQuantity:34(bunch)}"' +
+        '"logQuantity":"50(kilogram)", "logUser":"superuserjulz@example.com"}';
+
+    const enc = new TextEncoder();
+    const note = enc.encode(supplyChainData);
+    let txn = {
+        "from": recoveredAccount1.addr,
+        "to": recoveredAccount2.addr,
+        // comment out the next line to use suggested fee
+        "fee": 1000, // transactions only cost 1/1000th of an Algo (0.001).
+        "amount": 0, // 0 algos
+        "firstRound": params.firstRound,
+        "lastRound": params.lastRound,
+        "genesisID": params.genesisID,
+        "genesisHash": params.genesisHash,
+        "note": note
+    }
+
+    // sign transaction using private key
+    let signedTxn = algosdk.signTransaction(txn, recoveredAccount1.sk);
+
+    // submit the signed transaction to the network
+    let sendTx = await algodclient.sendRawTransaction(signedTxn.blob).do();
+
+    console.log("Transaction: " + sendTx.txId);
+
+    // Wait for confirmation
+   let confirmedTxn = await waitForConfirmation(algodclient, sendTx.txId, 4);
+
+   // get the completed transaction
+   console.log("Transaction " + sendTx.txId + " confirmed in round " + confirmedTxn["confirmed-round"]);
+
+   var txnNote = new TextDecoder().decode(confirmedTxn.txn.txn.note);
+   console.log("Note field: ", txnNote);
+
+   console.log("Transaction Amount: %d microAlgos", confirmedTxn.txn.txn.amt);
+   console.log("Transaction Fee: %d microAlgos", confirmedTxn.txn.txn.fee);
+
+   console.log("Now checking balances...");
+   // get information on accounts e.g. balances
+   let account_info = (await algodclient.accountInformation(recoveredAccount1.addr).do());
+   let acct_string = JSON.stringify(account_info);
+   console.log("Account 1 Info: " + acct_string);
+   console.log("Balance of account 1 in microAlgos: " + JSON.stringify(account_info.amount));
+
+   account_info = (await algodclient.accountInformation(recoveredAccount2.addr).do());
+   acct_string = JSON.stringify(account_info);
+   console.log("Account 2 Info: " + acct_string);
+   console.log("Balance of account 2: " + JSON.stringify(account_info.amount));
+
+})().catch( e => {
+    console.log(e)
+})
+//------/Log supply chain data (handover/storage) and check balance------\\
